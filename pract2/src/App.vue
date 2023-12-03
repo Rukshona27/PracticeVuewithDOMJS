7af534665b34577a4ce492e4f53fef4513e3105e
Rukshona27 <rukshona.lu@gmail.com>
2023-12-03 21:54:00 +0300

    решение некоторых задач

---
+++ pract2/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="App">
+    <Helloworld/>
+  </div>
+</template>
+
+<script>
+import Helloworld from "./components/HelloWorld.vue";
+
+export default{
+  components: {
+    Helloworld
+  }
+};
+</script>
---
+++ practVueJs/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="App">
+    <Helloworld/>
+  </div>
+</template>
+
+<script>
+import Helloworld from "./components/component.vue";
+
+export default{
+  components: {
+    Helloworld
+  }
+};
+</script>
