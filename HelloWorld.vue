28543238ae953bb84fd169366851dece4a2aa905
Rukhshona27 <137923532+Rukshona27@users.noreply.github.com>
2023-12-07 16:32:54 +0300

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+	<div class="aa">
+		<input v-model="num">
+	<input  v-model="num2">
+	<input  v-model="num3">
+	<p>{{ res}}</p>
+	<button @click="resBtn">btn</button>
+	</div>
+</template>
+
+
+<script>
+  export default{
+    data() {
+	return{
+		num:0,
+		num2:0,
+		num3:0,
+		arr:[],
+	}
+	},
+	methods:{
+		resBtn:function(){
+			let i = 0;
+			this.arr = (this.num).split('-')
+			while (i<this.arr.length) {
+				this.num = this.arr[i];
+				i++;
+				this.num2 = this.arr[i];
+				i++;
+				this.num3 = this.arr[i];
+				i++;
+			}
+		}
+	}
+}
+</script>
+<style>
+	.aa{
+		font-size: 2em;
+	}
+</style>
+
