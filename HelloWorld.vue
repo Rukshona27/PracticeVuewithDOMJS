9e7a11572dd5d6bea202131391d63eaab18a13de
Rukhshona27 <137923532+Rukshona27@users.noreply.github.com>
2023-12-12 18:11:52 +0300

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+	  <p>Цена за литр топлива: {{ fuel.pricePerLiter }}</p>
+	  <p>Количество литров: {{ fuel.liters }}</p>
+	  <p>Итоговая стоимость: {{totalCost}}</p>
+	  <button @click="changeLiters">Изменить количество топлива</button>
+	</div>
+  </template>
+  
+  
+  
+  <script>
+  export default{
+  data(){
+	return{
+	  fuel: {
+		pricePerLiter: 50,
+		liters: 15,
+	   }
+	  };
+	},
+	computed: {
+	  totalCost() {
+		return this.fuel.pricePerLiter*this.fuel.liters;
+	  }
+	},
+	methods: {
+	  changeLiters() {
+		let newLiters = prompt('Введите новое количество топлива:');
+		if (newLiters !==null || !isNaN(newLiters)){
+		  this.fuel.liters = parseFloat(newLiters);
+		}
+	  }
+	}
+  }
+</script>
+<style>
+	.aa{
+		font-size: 2em;
+	}
+</style>
+
