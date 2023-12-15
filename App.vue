c60767b8585e1a3308a9a9866ab6d5c0f14692d6
Rukhshona27 <137923532+Rukshona27@users.noreply.github.com>
2023-12-15 20:12:28 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <HelloWorld 
+  v-for="user of users" 
+  :name="user.name" 
+  :salary="user.salary" 
+  :age="user.age" 
+  :key="user.id"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+  export default {
+    data() {
+	  return {
+		users: [
+			{
+				id: 1,
+				name: 'name1',
+				salary: 100,
+				age: 30,
+			},
+			{
+				id: 2,
+				name: 'name2',
+				salary: 200,
+				age: 40,
+			},
+			{
+				id: 3,
+				name: 'name3',
+				salary: 300,
+				age: 50,
+			},
+		],
+	}
+},
+components:{HelloWorld}
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+ <p>{{ name }}</p>
+ <p>  {{ salary }}</p> 
+ <p>  {{ age }} </p>
+</template>
+
+<script>
+  export default{
+    props:{
+      name: String,
+      age:Number,
+      salary:Number,
+      id: Number
+    },
+    data(){
+      return {
+      }
+    }
+  }
+
+</script>
+
+
+<style scoped>
+
+</style>
