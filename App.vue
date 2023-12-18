4aa1ddb7c0105493a36f371dc5724ef3e9925504
Rukhshona27 <137923532+Rukshona27@users.noreply.github.com>
2023-12-18 19:14:00 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import FirstComp from './components/FirstComp.vue';
+
+
+</script>
+
+<template>
+  <FirstComp/>
+  <P>Реактивность - мгновенное изменение отображаемых данных на странице </P>
+</template>
