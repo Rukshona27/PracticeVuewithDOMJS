96cc0a0f042fa75eab7dd900680ed14ce2002b97
Rukhshona27 <137923532+Rukshona27@users.noreply.github.com>
2023-12-18 15:04:29 +0300

    Add files via upload

---
+++ FirstComp.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="aa">
+    <input type="checkbox" v-model="arr" value="v1" id="checkbox">
+    <label for="checkbox">v1</label>
+	<input type="checkbox" v-model="arr" value="v2" id="checkbox">
+    <label for="checkbox">v2</label>
+	<input type="checkbox" v-model="arr" value="v3" id="checkbox">
+    <label for="checkbox">v3</label>
+    <ul>
+        <li v-for="el in arr">{{ el }}</li>
+    </ul>
+   </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                arr: [],
+            }
+        }
+    }
+</script>
+
+<style>
+.aa{
+    font-size: 2em;
+};
+hr{
+    color:blueviolet
+}
+</style>
