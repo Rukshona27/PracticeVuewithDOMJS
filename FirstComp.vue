44589d06894ed24f3f7187e7e355dc05dbf36062
Rukhshona27 <137923532+Rukshona27@users.noreply.github.com>
2023-12-18 19:13:06 +0300

    Add files via upload

---
+++ FirstComp.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="aa">
+    <input type="radio" value="v1" v-model="choice" >
+    <input type="radio" value="v2" v-model="choice" >
+    <input type="radio" value="v3" v-model="choice" >
+    <input type="radio" value="v4" v-model="choice" >
+    <p>you choiced: {{ choice }}</p>
+    <p>Only one to choose from</p>
+   </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                choice:''
+            }
+        }
+    }
+</script>
+
+<style>
+.aa{
+    font-size: 2em;
+};
+hr{
+    color:blueviolet
+}
+</style>
